71c7bbd8c15f29c80afebf2f81630a2e6cdc5ee6
aliquodfahriam <rccrdpl6@gmail.com>
2023-12-04 11:50:54 +0100

    new inference and batch size 1 model

--- inferencePy/inference.py
+++ inferencePy/inference.py
@@ -2,8 +2,9 @@
 import tflite_runtime
 from tflite_runtime.interpreter import Interpreter 
 import numpy as np
+import time
 
-interpreter = Interpreter("../small_lstm_quad.tflite")
+interpreter = Interpreter("../small_lstm_batch_1_quad.tflite")
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
@@ -12,12 +13,16 @@ processed_test_data = np.float32(np.load("../processed_test_data_np.npy"))
 
 
 input_shape = input_details[0]['shape']
-input_data = processed_test_data[:256, :30, :14]
+input_data = processed_test_data[10:11, :30, :14]
 interpreter.set_tensor(input_details[0]['index'], input_data)
-
+start_time = time.time()
 interpreter.invoke()
+end_time = time.time()
+
+inference_time = (end_time - start_time)*1e6 #per avere il tempo in microsecondi
 
 print("Input_shape = ", input_shape)
 
 output_data = interpreter.get_tensor(output_details[0]['index'])
 print(output_data)
+print("Inference time: ", inference_time)

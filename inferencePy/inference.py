df553ee0395d528f17d74533261e8b8c2487d8ca
aliquodfahriam <rccrdpl6@gmail.com>
2023-12-04 12:39:52 +0100

    updated inference, added inference time inference_time_calculator

--- inferencePy/inference.py
+++ inferencePy/inference.py
@@ -2,7 +2,7 @@
 import tflite_runtime
 from tflite_runtime.interpreter import Interpreter 
 import numpy as np
-import time
+from inference_time_calculator import inference_time_calculator
 
 interpreter = Interpreter("../small_lstm_batch_1_quad.tflite")
 interpreter.allocate_tensors()
@@ -15,14 +15,12 @@ processed_test_data = np.float32(np.load("../processed_test_data_np.npy"))
 input_shape = input_details[0]['shape']
 input_data = processed_test_data[10:11, :30, :14]
 interpreter.set_tensor(input_details[0]['index'], input_data)
-start_time = time.time()
 interpreter.invoke()
-end_time = time.time()
-
-inference_time = (end_time - start_time)*1e6 #per avere il tempo in microsecondi
 
 print("Input_shape = ", input_shape)
 
 output_data = interpreter.get_tensor(output_details[0]['index'])
 print(output_data)
-print("Inference time: ", inference_time)
+
+print('Media tempo inferenza: ', inference_time_calculator(interpreter, processed_test_data, input_details))
+      
